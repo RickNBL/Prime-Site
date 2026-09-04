d6ebab73c4d31a9be05439c0df9a8f97aac2b2e8
RickNBl <floyd2k20@gmail.com>
2026-09-04 15:30:53 -0300

    php

--- config/site.php
+++ config/site.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Configuração do formulário.
- * Antes de publicar, substitua os e-mails abaixo por endereços reais do seu domínio.
- * Este arquivo não precisa conter senha de e-mail.
- */
 const SITE_NAME = 'Prime Terceirizados';
 const CONTACT_EMAIL = 'CONFIGURE@SEU-DOMINIO.COM.BR';
 const FROM_EMAIL = 'site@SEU-DOMINIO.COM.BR';
